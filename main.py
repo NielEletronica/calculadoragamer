3c71db137ba2dcf8b99c92eb8bdbca66728cd40c
NielEletronica <dniel150@proton.me>
2024-05-29 11:41:04 +0000

    added main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 def code():
-  x = int(input("Digite um número: "))
-  z = int (input("Digite outro número: "))
-  
-  method = int(input("Escolha o método desejado: 1 - Soma, 2 - Subtração, 3 - Multiplicação, 4 Divisão: "))
-  
-  if method == 1:
-    print(x+z)
-  elif method == 2:
-    print(x-z)
-  elif method == 3:
-    print(x*z)
-  elif method == 4:
-    print(x/z)
-  
-  restart = int(input("Deseja reiniciar? 1 - Sim, 2 - Não: "))
-  
-  if restart == 1:
-    code()
-  else:
-    print("Obrigado por usar nosso programa!")
+  try:
+    x = int(input("Digite um número: "))
+    z = int(input("Digite outro número: "))
+
+
+
+    method = int(input("Escolha o método desejado: 1 - Soma, 2 - Subtração, 3 - Multiplicação, 4 Divisão: "))
+
+    if method == 1:
+      print(x+z)
+    elif method == 2:
+      print(x-z)
+    elif method == 3:
+      print(x*z)
+    elif method == 4:
+      print(x/z)
+    else:
+      print("Hello World")
+    restart = int(input("Deseja reiniciar? 1 - Sim, 2 - Não: "))
+
+    if restart == 1:
+      code()
+    else:
+      print("Obrigado por usar nosso programa!")
+  except ValueError:
+    print("Hello World")
 code()
